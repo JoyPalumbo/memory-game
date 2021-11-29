f7b2d9e6be0015aece489e95387d83271434a3e5
Joy <greeniejoy@gmail.com>
2021-11-29 15:26:11 -0600

    Adde cat images and image data file

---
+++ src/data.js
@@ -0,0 +1,11 @@
+export const images = [
+    '../public/images/cat1',
+    '../public/images/cat2',
+    '../public/images/cat3',
+    '../public/images/cat4',
+    '../public/images/cat5',
+    '../public/images/cat6',
+    '../public/images/cat7',
+    '../public/images/cat8',
+    '../public/images/cat9'
+]
